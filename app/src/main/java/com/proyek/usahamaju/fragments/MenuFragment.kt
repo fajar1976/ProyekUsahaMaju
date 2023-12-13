a11ed82baeca613d7fcfc4994e872a6acef6974e
Fajar Pahmi Padilah <pahmipadilah76@gmail.com>
2023-12-13 07:11:20 +0700

    Update

--- app/src/main/java/com/proyek/usahamaju/fragments/MenuFragment.kt
+++ app/src/main/java/com/proyek/usahamaju/fragments/MenuFragment.kt
@@ -1,11 +1,15 @@
 package com.proyek.usahamaju.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.proyek.usahamaju.R
+import com.proyek.usahamaju.databinding.FragmentHomeBinding
+import com.proyek.usahamaju.databinding.FragmentMenuBinding
+import kotlinx.android.synthetic.main.fragment_menu.bt_keluar
 
 class MenuFragment : Fragment() {
 
@@ -13,10 +17,18 @@ class MenuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false)
+
+
+
+
+
     }
+
+
 
 
 }
